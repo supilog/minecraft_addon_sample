2754158674a57dc40318abc460e15b89b5e38880
supilog <supi.gm+supilog@gmail.com>
2025-06-17 10:42:51 +0900

    update give_item

--- behaviors/give_item/scripts/main.js
+++ behaviors/give_item/scripts/main.js
@@ -2,6 +2,6 @@ import * as server from '@minecraft/server';
 
 server.world.beforeEvents.itemUse.subscribe((eventData) => {
     if (eventData.itemStack.typeId === 'minecraft:stick') {
-        eventData.source.runCommand('give @s minecraft:diamond 1');
+        eventData.source.runCommand('give @a diamond 1');
     }
 });
